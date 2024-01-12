bb5adf377c4b415410fe671f750fdc7984fea83f
linuxpedro <pdro.dev97@gmail.com>
2024-01-12 07:32:49 -0300

    feat: property binding

--- angular-project/src/app/components/template/binding/binding.component.ts
+++ angular-project/src/app/components/template/binding/binding.component.ts
@@ -11,6 +11,14 @@ export class BindingComponent {
   public prop = 'Esse é um exemplo de propriedade'
   public example = '{{assim que se usa}}'
   public nmb1: number = 12
+  public isDisable = true
+  public srcValue = 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png'
+
+  constructor() {
+    setTimeout(() => {
+      this.prop = 'Nova prop'
+    }, 2000);
+  }
 
   public sum(n1: number, n2: number) {
     return n1 + n2
