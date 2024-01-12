c4fd9e289b2e1eed9a9f2257ddf06c9decb0477f
linuxpedro <pdro.dev97@gmail.com>
2024-01-12 07:48:12 -0300

    feat: attr binding

--- angular-project/src/app/components/template/binding/binding.component.ts
+++ angular-project/src/app/components/template/binding/binding.component.ts
@@ -13,7 +13,8 @@ export class BindingComponent {
   public nmb1: number = 12
   public isDisable = true
   public srcValue = 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png'
-
+  public soma = 'Soma'
+  
   constructor() {
     setTimeout(() => {
       this.prop = 'Nova prop'
