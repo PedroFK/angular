54b5e59446e5cb6e595c4b8e8e31692291a3f610
linuxpedro <pdro.dev97@gmail.com>
2024-02-14 22:23:42 -0300

    updates

--- angular-project/src/app/components/template/binding/binding.component.ts
+++ angular-project/src/app/components/template/binding/binding.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-binding',
@@ -9,11 +10,16 @@ import { Component } from '@angular/core';
 })
 export class BindingComponent {
   public prop = 'Esse é um exemplo de propriedade'
+  public name = 'Pedro'
   public example = '{{assim que se usa}}'
+  public age = 20
   public nmb1: number = 12
   public isDisable = true
   public srcValue = 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png'
   public soma = 'Soma'
+  public testeDeCor = 'Muda de cor conforme a idade'
+
+  public isTextDecorated = this.nmb1 >=10 ? 'underline' : 'none'
   
   constructor() {
     setTimeout(() => {
@@ -21,7 +27,22 @@ export class BindingComponent {
     }, 2000);
   }
 
-  public sum(n1: number, n2: number) {
-    return n1 + n2
+  public sum() {
+    return this.age++;
+  }
+
+  public sub() {
+    return this.age--
+  }
+
+  public onKeyDown(event: Event) {
+    return console.log(event)
+  }
+
+  public onMouseMove(event: MouseEvent) {
+    return console.log({
+      clientX: event.clientX,
+      clientY: event.clientY
+    })
   }
 }
