60a9722fa79ea53c64a51e54758af8ddfea6aab0
linuxpedro <pdro.dev97@gmail.com>
2024-02-15 08:57:41 -0300

    Diretivas de Atributos

--- angular-project/src/app/components/template/binding/binding.component.ts
+++ angular-project/src/app/components/template/binding/binding.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-binding',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './binding.component.html',
   styleUrl: './binding.component.scss'
 })
@@ -14,7 +15,8 @@ export class BindingComponent {
   public example = '{{assim que se usa}}'
   public age = 20
   public nmb1: number = 12
-  public isDisable = true
+  public isDisable = false
+  public isEnable = true
   public srcValue = 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png'
   public soma = 'Soma'
   public testeDeCor = 'Muda de cor conforme a idade'
