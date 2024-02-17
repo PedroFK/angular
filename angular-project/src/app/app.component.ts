3213272a9412167872cf78287cf37ba4c413fe8f
linuxpedro <pdro.dev97@gmail.com>
2024-02-17 20:18:36 -0300

    fix: main page

--- angular-project/src/app/app.component.ts
+++ angular-project/src/app/app.component.ts
@@ -5,15 +5,19 @@ import { RouterOutlet } from '@angular/router';
 // Component
 import { NewComponent } from './components/new/new.component';
 import { BindingComponent } from './components/template/binding/binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, BindingComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, BindingComponent, TemplateVariablesComponent],
   template: `
   <div class="dark">
-    <app-new></app-new>
-    <app-binding></app-binding>
+  <!-- 
+  <app-new></app-new>
+  <app-binding></app-binding>
+  -->
+    <app-template-variables />
   </div>
   
   `,
