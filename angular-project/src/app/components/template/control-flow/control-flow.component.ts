0ef02f4f502ac04077266cd29027f052c172683e
linuxpedro <pdro.dev97@gmail.com>
2024-02-18 09:55:19 -0300

    feat: for list

--- angular-project/src/app/components/template/control-flow/control-flow.component.ts
+++ angular-project/src/app/components/template/control-flow/control-flow.component.ts
@@ -1,14 +1,28 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [AsyncPipe, CommonModule],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.scss'
 })
 export class ControlFlowComponent {
   public isTrue = true
   public isFalse = false
+
+  public itens: Array<{name: string}> = [
+    { name: 'Pedro'},
+    { name: 'Carlos'},
+    { name: 'Jandira'},
+    { name: 'Gabriela'},
+  ]
+
+  public loadingData$: Observable<string[]> = of([
+    'carro',
+    'casa',
+    'moto'
+  ]).pipe(delay(3000))
 }
